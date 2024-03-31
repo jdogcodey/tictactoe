ef924857c90ae6b12f56e3560bdcb17f89aba707
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-04-01 00:09:29 +0100

    Made the game playable

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const GameBoard = (() => {
   const gameBoard = [];
   for (let i = 0; i <= 8; i++) {
     gameBoard[i] = `${i}`;
-    gameBoard[i] = document
-      .querySelector(`#space-${i}`)
-      .addEventListener("click", () => {
-        console.log(`button ${i} pressed`);
-      });
+    //   .addEventListener("click", () => {
+    //     console.log(`button ${i} pressed`);
+    //   });
   }
+  console.log(gameBoard);
   return gameBoard;
 })();
+console.log(GameBoard);
 
 const playGame = () => {
   // Factory Function to create a new player
@@ -21,28 +21,48 @@ const playGame = () => {
     let addScore = () => score++;
     return { name, playPiece, runningScore, addScore };
   }
-  //Create player one and two
+  //Create player one and two and initiate playPiece
   const playerOne = createPlayer(1, `X`);
   const playerTwo = createPlayer(2, `O`);
   console.log(playerOne);
   console.log(playerTwo);
+  let playPiece = "X";
+
+  const buttonClick = (() => {
+    for (let i = 0; i <= 8; i++) {
+      document.querySelector(`#space-${i}`).addEventListener("click", () => {
+        if (playPiece === "X") {
+          playPiece = "O";
+          document.querySelector(`#space-${i}`).textContent = "X";
+          GameBoard[i] = "X";
+          testWin();
+        } else {
+          playPiece = "X";
+          document.querySelector(`#space-${i}`).textContent = "O";
+          GameBoard[i] = "O";
+          testWin();
+        }
+      });
+    }
+  })();
+
   // Win criteria
-  if (
-    (GameBoard[0] === GameBoard[1] && GameBoard[0] === GameBoard[2]) ||
-    (GameBoard[3] === GameBoard[4] && GameBoard[3] === GameBoard[5]) ||
-    (GameBoard[6] === GameBoard[7] && GameBoard[6] === GameBoard[8]) ||
-    (GameBoard[0] === GameBoard[4] && GameBoard[0] === GameBoard[8]) ||
-    (GameBoard[2] === GameBoard[4] && GameBoard[2] === GameBoard[6]) ||
-    (GameBoard[0] === GameBoard[3] && GameBoard[0] === GameBoard[6]) ||
-    (GameBoard[1] === GameBoard[4] && GameBoard[1] === GameBoard[7]) ||
-    (GameBoard[2] === GameBoard[5] && GameBoard[2] === GameBoard[8])
-  ) {
-    console.log("Game Won");
+  function testWin() {
+    if (
+      (GameBoard[0] === GameBoard[1] && GameBoard[0] === GameBoard[2]) ||
+      (GameBoard[3] === GameBoard[4] && GameBoard[3] === GameBoard[5]) ||
+      (GameBoard[6] === GameBoard[7] && GameBoard[6] === GameBoard[8]) ||
+      (GameBoard[0] === GameBoard[4] && GameBoard[0] === GameBoard[8]) ||
+      (GameBoard[2] === GameBoard[4] && GameBoard[2] === GameBoard[6]) ||
+      (GameBoard[0] === GameBoard[3] && GameBoard[0] === GameBoard[6]) ||
+      (GameBoard[1] === GameBoard[4] && GameBoard[1] === GameBoard[7]) ||
+      (GameBoard[2] === GameBoard[5] && GameBoard[2] === GameBoard[8])
+    ) {
+      console.log("Game Won");
+    }
   }
 };
 
-//create two players by repeating code using factory function for each player
-//player contains name, X/O score
-//player contains factory function to increase score by 1
-//create gameboard with 9 array items created using factory function
-//Create playgame that goes from players1 to player2 back and forth until a row is created - then announces winner
+// Add a variable that updates - when even is X, when odd is O
+// Make it so on button press the text content updates to X/O, removes the button listener (might need to move this all down into the game)
+//
