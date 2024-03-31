54cbb2614283f31c66c964b25a6ac3d3281fa324
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-03-31 23:26:51 +0100

    Add grid that knows where you have pressed

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 const GameBoard = (() => {
   const gameBoard = [];
-  for (let i = 0; i <= 9; i++) {
-    gameBoard[i] = "";
+  for (let i = 0; i <= 8; i++) {
+    gameBoard[i] = `${i}`;
+    gameBoard[i] = document
+      .querySelector(`#space-${i}`)
+      .addEventListener("click", () => {
+        console.log(`button ${i} pressed`);
+      });
   }
-  console.log(gameBoard);
+  return gameBoard;
 })();
 
-function createPlayer(playerNumber, playerPiece) {
-  const name = prompt(`Player ${playerNumber} name`);
-  let score = 0;
-  const playPiece = playerPiece;
-  let runningScore = () => score;
-  let addScore = () => score++;
-  return { name, playPiece, runningScore, addScore };
-}
-
 const playGame = () => {
+  // Factory Function to create a new player
+  function createPlayer(playerNumber, playerPiece) {
+    const name = prompt(`Player ${playerNumber} name`);
+    let score = 0;
+    const playPiece = playerPiece;
+    let runningScore = () => score;
+    let addScore = () => score++;
+    return { name, playPiece, runningScore, addScore };
+  }
+  //Create player one and two
   const playerOne = createPlayer(1, `X`);
   const playerTwo = createPlayer(2, `O`);
   console.log(playerOne);
   console.log(playerTwo);
+  // Win criteria
+  if (
+    (GameBoard[0] === GameBoard[1] && GameBoard[0] === GameBoard[2]) ||
+    (GameBoard[3] === GameBoard[4] && GameBoard[3] === GameBoard[5]) ||
+    (GameBoard[6] === GameBoard[7] && GameBoard[6] === GameBoard[8]) ||
+    (GameBoard[0] === GameBoard[4] && GameBoard[0] === GameBoard[8]) ||
+    (GameBoard[2] === GameBoard[4] && GameBoard[2] === GameBoard[6]) ||
+    (GameBoard[0] === GameBoard[3] && GameBoard[0] === GameBoard[6]) ||
+    (GameBoard[1] === GameBoard[4] && GameBoard[1] === GameBoard[7]) ||
+    (GameBoard[2] === GameBoard[5] && GameBoard[2] === GameBoard[8])
+  ) {
+    console.log("Game Won");
+  }
 };
 
 //create two players by repeating code using factory function for each player
